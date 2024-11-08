c98cd9bea26231cf1afde55789f05d29cf3cfc28
남정욱 <wjddnr3304@gmail.com>
2024-11-08 14:06:23 +0900

    완성

--- Account/Program.cs
+++ Account/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace Account
 {
@@ -14,7 +15,80 @@ namespace Account
         // 계정금액 변동 등록
         static void AddSlip(slip[] s)
         {
-
+            string[] names1 = { "현금", "매출채권", "미수금", "건물", "기계장치" }; // 자산
+            string[] names2 = { "매입 채무", "차입금", "미지급금" }; // 부채
+            string[] names3 = { "자본금", "자본 잉여금", "이익 잉여금" }; // 자본
+            string[] names4 = { "매출액", "건물 입대료", "이자수익" }; // 수익
+            string[] names5 = { "매출 원가", "임차료", "급여", "광고선전비" }; //비용
+            Console.Write("날짜를 입력해 주세요(1~31)");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("계정유형을 입력하세요 자산 = 1, 부채 = 2, 자본 = 3, 수익 = 4, 비용 = 5> ");
+            s[n].type = int.Parse(Console.ReadLine());
+            switch (s[n].type)
+            {
+                case 1:
+                    {
+                        Console.Write("계정을 입력하세요");
+                        for (int i = 0; i < names1.Length; i++)
+                        {
+                            Console.Write("{0} + {1}", names1[i], i + 1);
+                            Console.Write(">");
+                            int id = int.Parse(Console.ReadLine());
+                            s[n].account = names1[i];
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("계정을 입력하세요");
+                        for (int i = 0; i < names2.Length; i++)
+                        {
+                            Console.Write("{0} + {1}", names2[i], i + 1);
+                            Console.Write(">");
+                            int id = int.Parse(Console.ReadLine());
+                            s[n].account = names2[i];
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.Write("계정을 입력하세요");
+                        for (int i = 0; i < names3.Length; i++)
+                        {
+                            Console.Write("{0} + {1}", names3[i], i + 1);
+                            Console.Write(">");
+                            int id = int.Parse(Console.ReadLine());
+                            s[n].account = names3[i];
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.Write("계정을 입력하세요");
+                        for (int i = 0; i < names4.Length; i++)
+                        {
+                            Console.Write("{0} + {1}", names4[i], i + 1);
+                            Console.Write(">");
+                            int id = int.Parse(Console.ReadLine());
+                            s[n].account = names4[i];
+                        }
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.Write("계정을 입력하세요");
+                        for (int i = 0; i < names5.Length; i++)
+                        {
+                            Console.Write("{0} + {1}", names5[i], i + 1);
+                            Console.Write(">");
+                            int id = int.Parse(Console.ReadLine());
+                            s[n].account = names5[i];
+                        }
+                        break;
+                    }
+            }
+            Console.Write("금액을 입력하세요 >");
+            s[n].amount = int.Parse(Console.ReadLine());
         }
         // 계정별로 정리하여 Hashtable을 만듦
         static Hashtable AccountSummary(slip[] s, int type)
@@ -35,20 +109,34 @@ namespace Account
                     }
                 }
             }
+                return summary;
         }
         // 계정 유형별로 정리하여 출력
         static void PrintAccountSummary(slip[] s, int type, string typename)
         {
-        
+            Hashtable summary = AccountSummary(s, type); // 계정별로 정리한다
+            int sum = 0;
+            foreach (DictionaryEntry cs in summary)
+            {
+                string account = (string)cs.Key;
+                int total = (int)cs.Value;
+                Console.WriteLine("{0} = {1}",account, total); // 계정이름과 금액 출력
+                sum = sum + total;
+            }
+            Console.WriteLine(" ** {0}총합 = {1}", typename, sum); // 전체 합을 출력
         }
         // 재무상태표 출력
         static void StatementFinancialPosition(slip[] s)
         {
-        
+            PrintAccountSummary(s, 1, "자산");
+            PrintAccountSummary(s, 2, "부채");
+            PrintAccountSummary(s, 3, "자본");
         }
         // 손익 계산서 출력
         static void StatementComprehensiveIncome(slip[] s)
         {
+            PrintAccountSummary(s, 4, "수익");
+            PrintAccountSummary(s, 5, "비용");
 
         }
         // 메뉴를 입력받아서 필요한 메소드를 호출하는 메인 메소드 정의
